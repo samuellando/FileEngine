bcc6716b07ae88e3cd24de454479ad37a4ec2889
Your Name <you@example.com>
2018-02-24 20:46:09 -0500

    Made cli into methods

--- src/interfaces/CommandLineInterface.java
+++ src/interfaces/CommandLineInterface.java
@@ -9,17 +9,13 @@ import fileengine.FileEngine;
 import java.util.Scanner;
 
 public class CommandLineInterface {
+  private static boolean kill = false;
+  private static String command;
+  private static Scanner keyboard = new Scanner(System.in);
+  private static FileEngine api = new FileEngine();
+
   /** Primitive interface, asks commands and uses api to complete them. */
   public static void main(String[] args) {
-    boolean kill = false;
-    String command;
-    Scanner keyboard = new Scanner(System.in);
-    FileEngine api = new FileEngine();
-    String stringBuffer;
-    boolean boolBuffer;
-    ProcessBuilder builder = new ProcessBuilder();
-    Process process;
-    mainLoop:
     while (!kill) {
       System.out.print("FE> ");
       command = keyboard.nextLine().toLowerCase();
@@ -30,44 +26,19 @@ public class CommandLineInterface {
           kill = true;
           break;
         case "add":
-          System.out.print("Enter directory path: ");
-          stringBuffer = keyboard.nextLine();
-          System.out.print("Sync with a remote git (y/n): ");
-          if (keyboard.nextLine().equalsIgnoreCase("y")) {
-            boolBuffer = true;
-          } else {
-            boolBuffer = false;
-          }
-          api.addDirectory(stringBuffer,boolBuffer);
+          add();
           break;
         case "remove":
         case "r":
-          System.out.print("Enter directory index: ");
-          api.removeDirectory(keyboard.nextInt());
-          keyboard.nextLine();
+          remove();
           break;
         case "tag":
         case "t":
-          for (int i = 0;i < api.listFiles(0).length;i++) {
-            System.out.println(api.listFiles(0)[i]);
-            System.out.print("Enter tag: ");
-            stringBuffer = keyboard.nextLine();
-            api.listFiles(0)[i].tag(stringBuffer);
-          }
+          tag();
           break;
         case "archive":
         case "a":
-          for (int i = 0;i < api.listFiles(0).length;i++) {
-            System.out.println(api.listFiles(0)[i]);
-            System.out.print("Archive (y/n): ");
-            if (keyboard.nextLine().equalsIgnoreCase("y")) {
-              for (int k = 0;k < api.getDirectories().length;k++) {
-                if (api.getDirectories()[k].getAbsolutePath().indexOf(api.listFiles(0)[i].toString().substring(0,api.listFiles(0)[i].toString().indexOf("`"))) > 0) {
-                  api.listFiles(0)[i].move(api.getDirectories()[k]);
-                }
-              }
-            }
-          }
+          archive();
           break;
         case "backup":
         case "b":
@@ -75,45 +46,112 @@ public class CommandLineInterface {
           break;
         case "work":
         case "w":
-          if (api.listFiles(1).length > 0) {
-            for (int i = 0;i < api.listFiles(1).length;i++) {
-              api.listFiles(1)[i].move(api.getDirectories()[0]);
-            }
-          } else {
-            for (int i = 0;i < api.listFiles(0).length;i++) {
-              api.listFiles(0)[i].move(api.getDirectories()[1]);
-            }
-          }
+          work();
           break;
         case "list":
         case "ls":
-          for (int i = 2;i < api.getDirectories().length;i++) {
-            System.out.println(api.getDirectories()[i].toString());
-          }
+          list();
           break;
         case "open":
         case "o":
-          System.out.print("Enter directory index: ");
-          stringBuffer = api.getDirectories()[keyboard.nextInt()].getAbsolutePath();
-          builder.command("nautilus",stringBuffer);
-          try {
-            process = builder.start();
-          } catch (Exception e) {
-            System.out.println("Error opening file manager.");
-          }
-          keyboard.nextLine();
-          break;
-        default:
-          System.out.println("Command not recongnized.");
+          open();
           break;
         case "clear":
         case "c":
           clear();
           break;
+        default:
+          System.out.println("Command not recongnized.");
+          break;
       }
     }
     keyboard.close();
   }
+
+  private static boolean add() {
+    String stringBuffer;
+    boolean boolBuffer;
+    System.out.print("Enter directory path: ");
+    stringBuffer = keyboard.nextLine();
+    System.out.print("Sync with a remote git (y/n): ");
+    if (keyboard.nextLine().equalsIgnoreCase("y")) {
+      boolBuffer = true;
+    } else {
+      boolBuffer = false;
+    }
+    api.addDirectory(stringBuffer,boolBuffer);
+    return true;
+  }
+
+  private static boolean remove() {
+    System.out.print("Enter directory index: ");
+    api.removeDirectory(keyboard.nextInt());
+    keyboard.nextLine();
+    return true;
+  }
+
+  private static boolean tag() {
+    String stringBuffer;
+    for (int i = 0;i < api.listFiles(0).length;i++) {
+      System.out.println(api.listFiles(0)[i]);
+      System.out.print("Enter tag: ");
+      stringBuffer = keyboard.nextLine();
+      api.listFiles(0)[i].tag(stringBuffer);
+    }
+    return true;
+  }
+
+  public static boolean archive() {
+    for (int i = 0;i < api.listFiles(0).length;i++) {
+      System.out.println(api.listFiles(0)[i]);
+      System.out.print("Archive (y/n): ");
+      if (keyboard.nextLine().equalsIgnoreCase("y")) {
+        for (int k = 0;k < api.getDirectories().length;k++) {
+          if (api.getDirectories()[k].getAbsolutePath().indexOf(api.listFiles(0)[i].toString().substring(0,api.listFiles(0)[i].toString().indexOf("`"))) > 0) {
+            api.listFiles(0)[i].move(api.getDirectories()[k]);
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  private static boolean work() {
+    if (api.listFiles(1).length > 0) {
+      for (int i = 0;i < api.listFiles(1).length;i++) {
+        api.listFiles(1)[i].move(api.getDirectories()[0]);
+      }
+    } else {
+      for (int i = 0;i < api.listFiles(0).length;i++) {
+        api.listFiles(0)[i].move(api.getDirectories()[1]);
+      }
+    }
+    return true;
+  }
+
+  private static boolean list() {
+    for (int i = 2;i < api.getDirectories().length;i++) {
+      System.out.println(api.getDirectories()[i].toString());
+    }
+    return true;
+  }
+
+  private static boolean open() {
+    String stringBuffer;
+    ProcessBuilder builder = new ProcessBuilder();
+    Process process;
+    System.out.print("Enter directory index: ");
+    stringBuffer = api.getDirectories()[keyboard.nextInt()].getAbsolutePath();
+    keyboard.nextLine();
+    builder.command("nautilus",stringBuffer);
+    try {
+      process = builder.start();
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
   private static boolean clear() {
     System.out.print("\033[H\033[2J");
     System.out.flush();
